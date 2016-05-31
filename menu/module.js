773bc587591f96948a5d8af42d46d7ad973d76d2
DaBitNG <davidnunogarcia@gmail.com>
2016-06-01 00:07:00 +0200

    Search tab added.

--- menu/module.js
+++ menu/module.js
@@ -1,23 +1,44 @@
+/**
+* Menu Module. 
+* @author DabitNG 
+*/
+
 angular.module('menu', [])
 
+	/**
+	* Global configuration for this module
+	* Configure your diferentet views here
+	* @param $stateProvider 
+	* @param $urlRouterProvider
+	* @return
+	*/
 	.config(function($stateProvider, $urlRouterProvider) {
-	  $stateProvider
+		$stateProvider
 
-	  .state('app', {
-	    url: '/app',
-	    abstract: true,
-	    templateUrl: 'modules/menu/templates/menu.html',
-	  })
+		.state('app', {
+			url: '/app',
+			abstract: true,
+			templateUrl: 'modules/menu/templates/menu.html',
+		})
 
-	  .state('app.home', {
-	    url: '/home',
-	    views: {
-	      'menuContent':{
-	        templateUrl: 'modules/menu/templates/home.html',
-	      }
-	    }
-	  });
+		.state('app.home', {
+			url: '/home',
+			views: {
+				'menuContent':{
+					templateUrl: 'modules/menu/templates/home.html',
+				}
+			}
+		})
 
-	  // if none of the above states are matched, use this as the fallback
-	  $urlRouterProvider.otherwise('/app/home');
+		.state('app.search', {
+			url: '/search',
+			views: {
+				'menuContent':{
+					templateUrl: 'modules/menu/templates/search.html',
+				}
+			}
+		});
+
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/app/home');
 });
